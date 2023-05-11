73ce11e0cfae177302b7d44eb13a8d4c7425628d
khimoli2052 <133236751+khimoli2052@users.noreply.github.com>
2023-05-11 19:51:07 +0545

    Add files via upload

---
+++ incomeExpenditure.php
@@ -0,0 +1,371 @@
+<?php require_once 'php_action/db_connect.php' ?>
+<?php require_once 'includes/header.php'; ?>
+
+<div class="row">
+	<div class="col-md-12">
+
+		<ol class="breadcrumb">
+		  <li><a href="dashboard.php">Home</a></li>		  
+		  <li class="active">IncomeExpenditure</li>
+		</ol>
+
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<div class="page-heading"> <i class="glyphicon glyphicon-edit"></i> Manage IncomeExpenditure</div>
+			</div> <!-- /panel-heading -->
+			<div class="panel-body">
+
+				<div class="remove-messages"></div>
+
+				<div class="div-action pull pull-right" style="padding-bottom:20px;">
+					<button class="btn btn-default button1" data-toggle="modal" id="addIncomeExpenditureModalBtn" data-target="#addIncomeModal"> <i class="glyphicon glyphicon-plus-sign"></i> Add </button>
+				</div> <!-- /div-action -->				
+				
+				<table class="table" id="manageIncomeExpenditureTable">
+					<thead>
+						<tr>							
+							<th>Account Name</th>
+							<th>Date</th>
+							<th>Category</th>
+							<th>Item</th>
+							<th>Income Rs.</th>
+							<th>Expenditure Rs.</th>
+							<th>Description</th>
+							<th>Status</th>
+							<!-- <th style="width:15%;">Options</th> -->
+						</tr>
+					</thead>
+				</table>
+				<!-- /table -->
+
+			</div> <!-- /panel-body -->
+		</div> <!-- /panel -->		
+	</div> <!-- /col-md-12 -->
+</div> <!-- /row -->
+
+
+<!-- add Income Expenditure-->
+<div class="modal fade" id="addIncomeModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+    	<form class="form-horizontal" id="submitIncomeForm" action="php_action/createIncomeExpenditure.php" method="POST" enctype="multipart/form-data">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title"><i class="fa fa-plus"></i> Income & Expenditure</h4>
+	      </div>
+
+	      <div class="modal-body" style="max-height:450px; overflow:auto;">
+
+	      	<div id="add-Income-messages"></div>
+     	           
+			<!-- /start-subgroup account name-->
+			  <div class="form-group">
+	        	<label for="accountName" class="col-sm-3 control-label">Account Name: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <select type="text" class="form-control" id="accountName" placeholder="Account Name" name="accountName" >
+				      	<option value="">~~SELECT~~</option>
+				      	<?php 
+				      	$sql = "SELECT account_id, account_name, account_active, account_status FROM accounts WHERE account_status = 1 AND account_active = 1";
+								$result = $connect->query($sql);
+
+								while($row = $result->fetch_array()) {
+									echo "<option value='".$row[0]."'>".$row[1]."</option>";
+								} // while
+								
+				      	?>
+				      </select>
+				    </div>
+	        </div> <!-- /form-group account-->	
+	    
+
+			<div class="form-group">
+	        	<label for="Date" class="col-sm-3 control-label">Date: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <input type="text" class="form-control" id="Date" placeholder="Quantity" name="Date" autocomplete="off">
+				    </div>
+	        </div> <!-- /form-date-->	
+        
+	        <div class="form-group">
+	        	<label for="categoryName" class="col-sm-3 control-label">Category Name: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <select type="text" class="form-control" id="categoryName" placeholder="Income Name" name="categoryName" >
+				      	<option value="">~~SELECT~~</option>
+				      	<?php 
+				      	$sql = "SELECT categories_id, categories_name, categories_active, categories_status FROM categories WHERE categories_status = 1 AND categories_active = 1";
+								$result = $connect->query($sql);
+
+								while($row = $result->fetch_array()) {
+									echo "<option value='".$row[0]."'>".$row[1]."</option>";
+								} // while
+								
+				      	?>
+				      </select>
+				    </div>
+	        </div> <!-- /form-group category-->					        	         	       
+
+			<div class="form-group">
+	        	<label for="itemName" class="col-sm-3 control-label">Item Name: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <select type="text" class="form-control" id="itemName" placeholder="Income Name" name="itemName" >
+				      	<option value="">~~SELECT~~</option>
+				      	<?php 
+				      	$sql = "SELECT item_id, item_name, item_active, item_status FROM item WHERE item_status = 1 AND item_active = 1";
+								$result = $connect->query($sql);
+
+								while($row = $result->fetch_array()) {
+									echo "<option value='".$row[0]."'>".$row[1]."</option>";
+								} // while
+								
+				      	?>
+				      </select>
+				    </div>
+	        </div> <!-- /form-group item-->	
+
+			<div class="form-group">
+	        	<label for="income" class="col-sm-3 control-label">Income Rs: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <input type="text" class="form-control" id="income" placeholder="income" name="income" autocomplete="off">
+				    </div>
+	        </div> <!-- /form-icome-->	   
+
+			<div class="form-group">
+	        	<label for="expenditure" class="col-sm-3 control-label">Expenditure Rs: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <input type="text" class="form-control" id="expenditure" placeholder="expenditure" name="expenditure" autocomplete="off">
+				    </div>
+	        </div> <!-- /form-expenditure-->
+
+			<div class="form-group">
+	        	<label for="description" class="col-sm-3 control-label">Description: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <input type="text" class="form-control" id="description" placeholder="descriptions" name="description" autocomplete="off">
+				    </div>
+	        </div> <!-- /form-description-->	
+
+	        <div class="form-group">
+	        	<label for="IncomeStatus" class="col-sm-3 control-label">Status: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <select class="form-control" id="IncomeStatus" name="IncomeStatus">
+				      	<option value="">~~SELECT~~</option>
+				      	<option value="1">Active</option>
+				      	<option value="2">InActive</option>
+				      </select>
+				    </div>
+	        </div> <!-- /form-group-->	         	        
+	      </div> <!-- /modal-body -->
+	      
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+	        
+	        <button type="submit" class="btn btn-primary" id="createIncomeBtn" data-loading-text="Loading..." autocomplete="off"> <i class="glyphicon glyphicon-ok-sign"></i> Save Changes</button>
+	      </div> <!-- /modal-footer -->	      
+     	</form> <!-- /.form -->	     
+    </div> <!-- /modal-content -->    
+  </div> <!-- /modal-dailog -->
+</div> 
+<!-- /add categories -->
+
+
+<!-- edit categories brand -->
+<div class="modal fade" id="editIncomeModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+    	    	
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title"><i class="fa fa-edit"></i> Edit Income</h4>
+	      </div>
+	      <div class="modal-body" style="max-height:450px; overflow:auto;">
+
+	      	<div class="div-loading">
+	      		<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+						<span class="sr-only">Loading...</span>
+	      	</div>
+
+	      	<div class="div-result">
+
+				  <!-- Nav tabs -->
+				  <ul class="nav nav-tabs" role="tablist">
+				    <li role="presentation" class="active"><a href="#photo" aria-controls="home" role="tab" data-toggle="tab">Photo</a></li>
+				    <li role="presentation"><a href="#IncomeInfo" aria-controls="profile" role="tab" data-toggle="tab">Income Info</a></li>    
+				  </ul>
+
+				  <!-- Tab panes -->
+				  <div class="tab-content">
+
+				  	
+				    <div role="tabpanel" class="tab-pane active" id="photo">
+				    	<form action="php_action/editIncomeImage.php" method="POST" id="updateIncomeImageForm" class="form-horizontal" enctype="multipart/form-data">
+
+				    	<br />
+				    	<div id="edit-IncomePhoto-messages"></div>
+
+				    	<div class="form-group">
+			        	<label for="editIncomeImage" class="col-sm-3 control-label">Income Image: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">							    				   
+						      <img src="" id="getIncomeImage" class="thumbnail" style="width:250px; height:250px;" />
+						    </div>
+			        </div> <!-- /form-group-->	     	           	       
+				    	
+			      	<div class="form-group">
+			        	<label for="editIncomeImage" class="col-sm-3 control-label">Select Photo: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">
+							    <!-- the avatar markup -->
+									<div id="kv-avatar-errors-1" class="center-block" style="display:none;"></div>							
+							    <div class="kv-avatar center-block">					        
+							        <input type="file" class="form-control" id="editIncomeImage" placeholder="Income Name" name="editIncomeImage" class="file-loading" style="width:auto;"/>
+							    </div>
+						      
+						    </div>
+			        </div> <!-- /form-group-->	     	           	       
+
+			        <div class="modal-footer editIncomePhotoFooter">
+				        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+				        
+				        <!-- <button type="submit" class="btn btn-success" id="editIncomeImageBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-ok-sign"></i> Save Changes</button> -->
+				      </div>
+				      <!-- /modal-footer -->
+				      </form>
+				      <!-- /form -->
+				    </div>
+				    <!-- Income image -->
+				    <div role="tabpanel" class="tab-pane" id="IncomeInfo">
+				    	<form class="form-horizontal" id="editIncomeForm" action="php_action/editIncome.php" method="POST">				    
+				    	<br />
+
+				    	<div id="edit-Income-messages"></div>
+
+				    	<div class="form-group">
+			        	<label for="editIncomeName" class="col-sm-3 control-label">Income Name: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">
+						      <input type="text" class="form-control" id="editIncomeName" placeholder="Income Name" name="editIncomeName" autocomplete="off">
+						    </div>
+			        </div> <!-- /form-group-->	    
+
+			        <div class="form-group">
+			        	<label for="editQuantity" class="col-sm-3 control-label">Quantity: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">
+						      <input type="text" class="form-control" id="editQuantity" placeholder="Quantity" name="editQuantity" autocomplete="off">
+						    </div>
+			        </div> <!-- /form-group-->	        	 
+
+			        <div class="form-group">
+			        	<label for="editRate" class="col-sm-3 control-label">Rate: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">
+						      <input type="text" class="form-control" id="editRate" placeholder="Rate" name="editRate" autocomplete="off">
+						    </div>
+			        </div> <!-- /form-group-->	     	        
+
+			        <div class="form-group">
+			        	<label for="editBrandName" class="col-sm-3 control-label">Brand Name: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">
+						      <select class="form-control" id="editBrandName" name="editBrandName">
+						      	<option value="">~~SELECT~~</option>
+						      	<?php 
+						      	$sql = "SELECT brand_id, brand_name, brand_active, brand_status FROM brands WHERE brand_status = 1 AND brand_active = 1";
+										$result = $connect->query($sql);
+
+										while($row = $result->fetch_array()) {
+											echo "<option value='".$row[0]."'>".$row[1]."</option>";
+										} // while
+										
+						      	?>
+						      </select>
+						    </div>
+			        </div> <!-- /form-group-->	
+
+			        <div class="form-group">
+			        	<label for="editCategoryName" class="col-sm-3 control-label">Category Name: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">
+						      <select type="text" class="form-control" id="editCategoryName" name="editCategoryName" >
+						      	<option value="">~~SELECT~~</option>
+						      	<?php 
+						      	$sql = "SELECT categories_id, categories_name, categories_active, categories_status FROM categories WHERE categories_status = 1 AND categories_active = 1";
+										$result = $connect->query($sql);
+
+										while($row = $result->fetch_array()) {
+											echo "<option value='".$row[0]."'>".$row[1]."</option>";
+										} // while
+										
+						      	?>
+						      </select>
+						    </div>
+			        </div> <!-- /form-group-->					        	         	       
+
+			        <div class="form-group">
+			        	<label for="editIncomeStatus" class="col-sm-3 control-label">Status: </label>
+			        	<label class="col-sm-1 control-label">: </label>
+						    <div class="col-sm-8">
+						      <select class="form-control" id="editIncomeStatus" name="editIncomeStatus">
+						      	<option value="">~~SELECT~~</option>
+						      	<option value="1">Available</option>
+						      	<option value="2">Not Available</option>
+						      </select>
+						    </div>
+			        </div> <!-- /form-group-->	         	        
+
+			        <div class="modal-footer editIncomeFooter">
+				        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+				        
+				        <button type="submit" class="btn btn-success" id="editIncomeBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-ok-sign"></i> Save Changes</button>
+				      </div> <!-- /modal-footer -->				     
+			        </form> <!-- /.form -->				     	
+				    </div>    
+				    <!-- /Income info -->
+				  </div>
+
+				</div>
+	      	
+	      </div> <!-- /modal-body -->
+	      	      
+     	
+    </div>
+    <!-- /modal-content -->
+  </div>
+  <!-- /modal-dailog -->
+</div>
+<!-- /categories brand -->
+
+<!-- categories brand -->
+<div class="modal fade" tabindex="-1" role="dialog" id="removeIncomeModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title"><i class="glyphicon glyphicon-trash"></i> Remove Income</h4>
+      </div>
+      <div class="modal-body">
+
+      	<div class="removeIncomeMessages"></div>
+
+        <p>Do you really want to remove ?</p>
+      </div>
+      <div class="modal-footer removeIncomeFooter">
+        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+        <button type="button" class="btn btn-primary" id="removeIncomeBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-ok-sign"></i> Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- /categories brand -->
+
+
+<script src="custom/js/Income.js"></script>
+
+<?php require_once 'includes/footer.php'; ?>
---
+++ items.php
@@ -0,0 +1,188 @@
+<?php require_once 'includes/header.php'; ?>
+
+
+<div class="row">
+	<div class="col-md-12">
+
+		<ol class="breadcrumb">
+		  <li><a href="dashboard.php">Home</a></li>		  
+		  <li class="active">Item</li>
+		</ol>
+
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<div class="page-heading"> <i class="glyphicon glyphicon-edit"></i> Manage Item</div>
+			</div> <!-- /panel-heading -->
+			<div class="panel-body">
+
+				<div class="remove-messages"></div>
+
+				<div class="div-action pull pull-right" style="padding-bottom:20px;">
+					<button class="btn btn-default button1" data-toggle="modal" id="additemModalBtn" data-target="#additemModal"> <i class="glyphicon glyphicon-plus-sign"></i> add Items </button>
+				</div> <!-- /div-action -->				
+				
+				<table class="table" id="manageitemTable">
+					<thead>
+						<tr>							
+							<th>Item Name</th>
+							<!-- <th>Categories Name</th>	 -->
+							<th>Status</th>
+							<th style="width:15%;">Options</th>
+						</tr>
+					</thead>
+				</table>
+				<!-- /table -->
+
+			</div> <!-- /panel-body -->
+		</div> <!-- /panel -->		
+	</div> <!-- /col-md-12 -->
+</div> <!-- /row -->
+
+
+<!-- add items -->
+<div class="modal fade" id="additemModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+    	<form class="form-horizontal" id="submititemForm" action="php_action/createItems.php" method="POST">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title"><i class="fa fa-plus"></i> add items</h4>
+	      </div>
+	      <div class="modal-body">
+
+	      	<div id="add-Items-messages"></div>
+
+	        <div class="form-group">
+	        	<label for="itemName" class="col-sm-4 control-label">Item Name: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-7">
+				      <input type="text" class="form-control" id="itemName" placeholder="Items Name" name="itemName" autocomplete="off">
+				    </div>
+	        </div> <!-- /start-Category-->	
+			<div class="form-group">
+	        	<label for="categoryName" class="col-sm-3 control-label">Category Name: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-8">
+				      <select type="text" class="form-control" id="categoryName" placeholder="Product Name" name="categoryName" >
+				      	<option value="">~~SELECT~~</option>
+				      	<?php 
+				      	$sql = "SELECT categories_id, categories_name, categories_active, categories_status FROM categories WHERE categories_status = 1 AND categories_active = 1";
+								$result = $connect->query($sql);
+
+								while($row = $result->fetch_array()) {
+									echo "<option value='".$row[0]."'>".$row[1]."</option>";
+								} // while
+								
+				      	?>
+				      </select>
+				    </div>
+	        </div><!-- /end-Category-->	
+			
+	        <div class="form-group">
+	        	<label for="itemStatus" class="col-sm-4 control-label">Status: </label>
+	        	<label class="col-sm-1 control-label">: </label>
+				    <div class="col-sm-7">
+				      <select class="form-control" id="itemStatus" name="itemStatus">
+				      	<option value="">~~SELECT~~</option>
+				      	<option value="1">Active</option>
+				      	<option value="2">InActive</option>
+				      </select>
+				    </div>
+	        </div> <!-- /form-group-->	         	        
+	      </div> <!-- /modal-body -->
+	      
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+	        
+	        <button type="submit" class="btn btn-primary" id="createitemBtn" data-loading-text="Loading..." autocomplete="off"> <i class="glyphicon glyphicon-ok-sign"></i> Save Changes</button>
+	      </div> <!-- /modal-footer -->	      
+     	</form> <!-- /.form -->	     
+    </div> <!-- /modal-content -->    
+  </div> <!-- /modal-dailog -->
+</div> 
+<!-- /add items -->
+
+
+<!-- edit Items brand -->
+<div class="modal fade" id="edititemModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+    	
+    	<form class="form-horizontal" id="edititemForm" action="php_action/editItems.php" method="POST">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title"><i class="fa fa-edit"></i> Edit Brand</h4>
+	      </div>
+	      <div class="modal-body">
+
+	      	<div id="edit-item-messages"></div>
+
+	      	<div class="modal-loading div-hide" style="width:50px; margin:auto;padding-top:50px; padding-bottom:50px;">
+						<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+						<span class="sr-only">Loading...</span>
+					</div>
+
+		      <div class="edit-item-result">
+		      	<div class="form-group">
+		        	<label for="edititemName" class="col-sm-4 control-label">Item Name: </label>
+		        	<label class="col-sm-1 control-label">: </label>
+					    <div class="col-sm-7">
+					      <input type="text" class="form-control" id="edititemName" placeholder="Item Name" name="edititemName" autocomplete="off">
+					    </div>
+		        </div> <!-- /form-group-->	         	        
+		        <div class="form-group">
+		        	<label for="editItemStatus" class="col-sm-4 control-label">Status: </label>
+		        	<label class="col-sm-1 control-label">: </label>
+					    <div class="col-sm-7">
+					      <select class="form-control" id="editItemsStatus" name="editItemStatus">
+					      	<option value="">~~SELECT~~</option>
+					      	<option value="1">Active</option>
+					      	<option value="2">InActive</option>
+					      </select>
+					    </div>
+		        </div> <!-- /form-group-->	 
+		      </div>         	        
+		      <!-- /edit brand result -->
+
+	      </div> <!-- /modal-body -->
+	      
+	      <div class="modal-footer editCategoriesFooter">
+	        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+	        
+	        <button type="submit" class="btn btn-success" id="editCategoriesBtn" data-loading-text="Loading..." autocomplete="off"> <i class="glyphicon glyphicon-ok-sign"></i> Save Changes</button>
+	      </div>
+	      <!-- /modal-footer -->
+     	</form>
+	     <!-- /.form -->
+    </div>
+    <!-- /modal-content -->
+  </div>
+  <!-- /modal-dailog -->
+</div>
+<!-- /categories brand -->
+
+<!-- item brand -->
+<div class="modal fade" tabindex="-1" role="dialog" id="removeitemModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title"><i class="glyphicon glyphicon-trash"></i> Remove Brand</h4>
+      </div>
+      <div class="modal-body">
+        <p>Do you really want to remove ?</p>
+      </div>
+      <div class="modal-footer removeitemFooter">
+        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+        <button type="button" class="btn btn-primary" id="removeitemBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-ok-sign"></i> Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- /item brand -->
+
+
+<script src="custom/js/item.js"></script>
+
+<?php require_once 'includes/footer.php'; ?>
